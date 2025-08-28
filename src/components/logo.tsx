cff96c594dd4efdc98e38ae9c68fa0e87cbf6f92
Emmanuel Lafenwa <leoemaxie@gmail.com>
2025-08-28 15:10:58 +0000

    i have added the images to the /public/images folder

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { Package } from 'lucide-react';
+import Image from 'next/image';
 
 type LogoProps = {
   className?: string;
@@ -10,9 +10,7 @@ type LogoProps = {
 export function Logo({ className, isTextVisible = true }: LogoProps) {
   return (
     <Link href="/" className={cn('flex items-center gap-2', className)}>
-      <div className="bg-primary rounded-md p-2">
-        <Package className="h-6 w-6 text-primary-foreground" />
-      </div>
+      <Image src="/images/logo.png" alt="Univend Logo" width={40} height={40} />
       {isTextVisible && (
         <span className="text-2xl font-bold font-headline text-slate-800">
           Univend

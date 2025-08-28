b6cf6eb4269c898345de8df3656a9d37534b32cc
Emmanuel Lafenwa <leoemaxie@gmail.com>
2025-08-28 20:39:30 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,7 @@ const nextConfig: NextConfig = {
         pathname: "/**",
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'avatar.vercel.sh',
         port: '',
         pathname: '/**',

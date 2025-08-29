974af141a3fddc626f62c4ae43938d921bdac5a3
Emmanuel Lafenwa <leoemaxie@gmail.com>
2025-08-29 23:12:18 +0000

    which of our brand color or outside of our brand color do you feel shoul

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import { Logo } from '@/components/logo';
 
 export default function Footer() {
   return (
-    <footer className="border-t">
+    <footer className="border-t bg-foreground text-background">
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
